106c05f81d66a79f0e0083a3f85e2398cb319659
yontonsoup <ygoldobin@gmail.com>
2018-10-01 22:13:44 -0700

    rendered the damn world map!

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 ogr2ogr \
   -f GeoJSON \
-  -where "ADM0_A3 IN ('ARM', 'AZE', 'BLR', 'EST', 'GEO', 'KAZ', 'KGZ', 'LVA', 'LTU', 'MDA', 'RUS', 'TJK', 'TKM', 'UKR', 'UZB')" \
-  subunitsSovietReal.json \
-  ne_10m_admin_0_map_subunits.shp
+  -where "ADM0_A3 IN (  'ARM', 'AZE', 'BLR', 'EST', 'GEO', 'KAZ', 'KGZ', 'LVA', 'LTU', 'MDA', 'RUS', 'TJK', 'TKM', 'UKR', 'UZB')" \
+  soviet.json \
+  ne_50m_admin_0_map_subunits.shp
 
   ogr2ogr \
   -f GeoJSON \
@@ -22,3 +22,24 @@ ogr2ogr \
 
 
   'ARM', 'AZE', 'BLR', 'EST', 'GEO', 'KAZ', 'KGZ', 'LVA', 'LTU', 'MDA', 'RUS', 'TJK', 'TKM', 'UKR', 'UZB'
+
+  ogr2ogr -f GeoJSON -t_srs EPSG:4326 -lco COORDINATE_PRECISION=6 
+ne_110m_admin_0_countries.geojson 
+/vsicurl/https://github.com/nvkelso/natural-earth-vector/raw/master/110m_cultural/ne_110m_admin_0_countries.shp
+
+
+ogr2ogr \
+  -f GeoJSON \
+  world.json \
+  ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp
+
+
+
+  // create map
+  ogr2ogr \
+  -f GeoJSON \
+  world.json \
+  ne_50m_admin_0_countries.shp
+
+  // convert to TOPOjson
+  topojson --id-property iso_a3 -p name -o world-topo.json world.json
