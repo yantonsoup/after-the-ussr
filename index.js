920ab4a517e9702cd4856bc23e0dcd36c12c3414
yantonsoup <ygoldobin@gmail.com>
2018-10-19 20:39:53 -0700

    put map into scroll_graphic!

--- index.js
+++ index.js
@@ -1,98 +1,129 @@
-window.onbeforeunload = function () {
+window.onbeforeunload = function() {
   window.scrollTo(0, 0);
-}
-// using d3 for convenience
-var container = d3.select('#scroll');
-var graphic = container.select('.scroll__graphic');
-var text = container.select('.scroll__text');
-var step = text.selectAll('.step');
+};
 
-// initialize the scrollama
-var scroller = scrollama();
+window.onload = function() {
+  // using d3 for convenience
+  var container = d3.select("#scroll");
+  var graphic = container.select(".scroll__graphic");
+  var text = container.select(".scroll__text");
+  var step = text.selectAll(".step");
 
-// generic window resize listener event
-function handleResize() {
-  // 1. update height of step elements
-  var stepHeight = Math.floor(window.innerHeight * 0.75);
-  step.style('height', stepHeight + 'px');
+  // initialize the scrollama
+  var scroller = scrollama();
 
-  // 2. update width/height of graphic element
-  var bodyWidth = d3.select('body').node().offsetWidth;
+  // generic window resize listener event
+  function handleResize() {
+    // 1. update height of step elements
+    var stepHeight = Math.floor(window.innerHeight * 0.75);
+    step.style("height", stepHeight + "px");
 
+    // 2. update width/height of graphic element
+    var bodyWidth = d3.select("body").node().offsetWidth;
+
+    var graphicMargin = 16 * 4;
+    var textWidth = text.node().offsetWidth;
+    var graphicWidth = container.node().offsetWidth - graphicMargin;
+    var graphicHeight = Math.floor(window.innerHeight / 2.4);
+    var graphicMarginTop = Math.floor(window.innerHeight / 30);
+
+    graphic
+      .style("width", graphicWidth + "px")
+      .style("height", graphicHeight + "px")
+      .style("top", graphicMarginTop + "px");
+
+    // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+  }
   var graphicMargin = 16 * 4;
-  var textWidth = text.node().offsetWidth;
-  var graphicWidth = container.node().offsetWidth - graphicMargin;
-  var graphicHeight = Math.floor(window.innerHeight / 2.4)
-  var graphicMarginTop = Math.floor(window.innerHeight / 30)
+  var mapWidth = container.node().offsetWidth - graphicMargin;
+  var mapHeight = Math.floor(window.innerHeight / 2.4);
 
-  graphic
-    .style('width', graphicWidth + 'px')
-    .style('height', graphicHeight + 'px')
-    .style('top', graphicMarginTop + 'px');
+    var width = 400,
+      height = 200;
+
+    var svg = d3.select(".scroll__graphic").append("svg")
+      .attr("width", mapWidth)
+      .attr("height", mapHeight);
+
+       console.warn('svg', svg)
+    
+    
+    d3.json("world.json", function (error, world) {
+      console.warn('loaded world', world)
+      if (error) return console.error(error);
+      var projection = d3.geo.mercator()
+      // .scale(500)
+      .translate([mapWidth / 2, mapHeight / 2]);
+
+      var pathGenerator = d3.geo.path().projection(projection);
+      svg.append("path")
+        .datum(world)
+        .attr("d", pathGenerator);
+    });
 
 
-  // 3. tell scrollama to update new element dimensions
-  scroller.resize();
-}
+  // scrollama event handlers
+  function handleStepEnter(response) {
+    console.warn("handleStepEnter", { response });
+    console.warn("handleStepEnter step index: response.index", response.index);
 
-// scrollama event handlers
-function handleStepEnter(response) {
-  console.warn('handleStepEnter', { response })
-  console.warn('handleStepEnter step index: response.index', response.index)
+    // response = { element, direction, index }
 
-  // response = { element, direction, index }
+    // add color to current step only
+    step.classed("is-active", function(d, i) {
+      return i === response.index;
+    });
 
-  // add color to current step only
-  step.classed('is-active', function (d, i) {
-    return i === response.index;
-  })
+    // update graphic based on step
+    graphic.select("p").text(response.index + 1);
+  }
 
-  // update graphic based on step
-  graphic.select('p').text(response.index + 1);
-}
+  function handleContainerEnter(response) {
+    d3.select(".intro__overline").classed("sticky_break", true);
+    console.warn({ handleContainerEnter });
+    // response = { direction }
+  }
 
-function handleContainerEnter(response) {
-  d3.select('.intro__overline').classed('sticky_break', true)
-  console.warn({ handleContainerEnter })
-  // response = { direction }
-}
+  function handleContainerExit(response) {
+    console.warn({ handleContainerExit });
 
-function handleContainerExit(response) {
-  console.warn({ handleContainerExit })
+    // response = { direction }
+  }
 
-  // response = { direction }
-}
+  function setupStickyfill() {
+    d3.selectAll(".sticky").each(function() {
+      Stickyfill.add(this);
+    });
+  }
 
-function setupStickyfill() {
-  d3.selectAll('.sticky').each(function () {
-    Stickyfill.add(this);
-  });
-}
+  function init() {
+    setupStickyfill();
 
-function init() {
-  setupStickyfill();
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize();
 
-  // 1. force a resize on load to ensure proper dimensions are sent to scrollama
-  handleResize();
+    // 2. setup the scroller passing options
+    // this will also initialize trigger observations
+    // 3. bind scrollama event handlers (this can be chained like below)
+    scroller
+      .setup({
+        container: "#scroll",
+        graphic: ".scroll__graphic",
+        text: ".scroll__text",
+        step: ".scroll__text .step",
+        debug: true,
+        offset: 0.5
+      })
+      .onStepEnter(handleStepEnter)
+      .onContainerEnter(handleContainerEnter)
+      .onContainerExit(handleContainerExit);
 
-  // 2. setup the scroller passing options
-  // this will also initialize trigger observations
-  // 3. bind scrollama event handlers (this can be chained like below)
-  scroller.setup({
-    container: '#scroll',
-    graphic: '.scroll__graphic',
-    text: '.scroll__text',
-    step: '.scroll__text .step',
-    debug: true,
-    offset: 0.5,
-  })
-    .onStepEnter(handleStepEnter)
-    .onContainerEnter(handleContainerEnter)
-    .onContainerExit(handleContainerExit);
+    // setup resize event
+    window.addEventListener("resize", handleResize);
+  }
 
-  // setup resize event
-  window.addEventListener('resize', handleResize);
-}
-
-// kick things off
-init();
+  // kick things off
+  init();
+  // initializeD3()
+};
