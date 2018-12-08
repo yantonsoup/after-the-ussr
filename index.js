9eda3ff6b19afd651a49f34c642aa522fdc60e08
yantonsoup <ygoldobin@gmail.com>
2018-12-07 20:30:33 -0800

    label placement

--- index.js
+++ index.js
@@ -145,20 +145,20 @@ function thirdAnimation() {
 }
 
 var sovietLabelShift = {
-  'ARM': {x: -12, y: 0}, 
-  'AZE': {x: -12, y: 0}, 
-  'BLR': {x: -12, y: 0}, 
+  'ARM': {x: -12, y: 2}, 
+  'AZE': {x: -8, y: 5}, 
+  'BLR': {x: -14, y: 4}, 
   'EST': {x: -12, y: 0}, 
-  'GEO': {x: -12, y: 0}, 
-  'KAZ': {x: 0, y: 0}, 
-  'KGZ': {x: -12, y: 0}, 
+  'GEO': {x: -13, y: 1}, 
+  'KAZ': {x: 14, y: 6}, 
+  'KGZ': {x: 5, y: 3}, 
   'LVA': {x: -12, y: 0}, 
   'LTU': {x: -14, y: 0}, 
-  'MDA': {x: -12, y: 0}, 
+  'MDA': {x: -12, y: 1}, 
   'RUS': {x: -40, y: 10}, 
-  'TJK': {x: -12, y: 0}, 
-  'TKM': {x: -12, y: 0}, 
-  'UKR': {x: 0, y: 0}, 
+  'TJK': {x: -4, y: 6}, 
+  'TKM': {x: -10, y: 8}, 
+  'UKR': {x: -9, y: 7}, 
   'UZB': {x: -12, y: 0}
 }
 
