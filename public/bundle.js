de24f3afa9206a4652aa1afee865f10af7995dcd
yantonsoup <ygoldobin@gmail.com>
2019-02-20 17:41:40 -0800

    clean up css

--- public/bundle.js
+++ public/bundle.js
@@ -1991,31 +1991,21 @@
     // Setup sizes for the graphic and steps
     var container = d3.select(".scroll");
     const boundingBox = container.node().getBoundingClientRect();
-    const {
-      width,
-      height
-    } = boundingBox;
     const text = container.select(".scroll-text");
     const textWidth = text.node().offsetWidth;
     const step = text.selectAll(".step");
-    const stepHeight = Math.floor(window.innerHeight * 1);
+    const stepHeight = Math.floor(window.innerHeight);
+    const halfPageHeight = Math.floor(window.innerHeight / 2);
+    const graphicMarginTop = Math.floor(window.innerHeight * 0.25);
     step.style("height", stepHeight + "px");
     text.selectAll(".step-two").style('height', '200px');
-    let graphicWidthHeight = width;
-    const halfPageHeight = Math.floor(window.innerHeight / 2); // make margin top for bar chart the size of the map container
-    // console.warn('graphic Width AND, height', graphic.node().offsetWidth)
 
-    const graphicMarginTop = Math.floor(window.innerHeight * 0.25); // scroll graphic is world map container
+    d3.select(".map-graphic-container").style("width", '100%').style("height", halfPageHeight + "px").style("top", graphicMarginTop + "px");
+    d3.select(".bar-graphic-container").style('top', halfPageHeight + 'px').style("width", '100%' + "px").style("height", halfPageHeight + "px"); // Use this to set the distance ofo the first step
 
-    d3.select(".map-graphic-container").style("width", graphicWidthHeight + "px").style("height", halfPageHeight + "px").style("top", graphicMarginTop + "px");
-    d3.select(".bar-graphic-container").style('top', halfPageHeight + 'px').style("width", graphicWidthHeight + "px").style("height", halfPageHeight + "px"); // Use this to set the distance ofo the first step
-
-    d3.select(".header-container").style("height", Math.floor(window.innerHeight * 1) + "px");
-    d3.select(".ussr-svg-container").style("width", textWidth + "px");
+    d3.select(".splash-container").style("height", Math.floor(window.innerHeight * 1) + "px");
     d3.select(".intro-block").style("width", textWidth + "px");
     d3.select(".name-block").style("width", textWidth + "px");
-    d3.select(".ussr-svg").style("height", 200 + "px");
-    d3.select(".ussr-svg").style("width", 200 + "px");
   }
 
   function loadMap() {
--- src/firstPaint.js
+++ src/firstPaint.js
@@ -9,38 +9,35 @@ export default function firstPaint () {
   const textWidth = text.node().offsetWidth;
 
   const step = text.selectAll(".step");
-  const stepHeight = Math.floor(window.innerHeight * 1);
+
+  const stepHeight = Math.floor(window.innerHeight);
+
+  const halfPageHeight = Math.floor(window.innerHeight/2)
+
+  const graphicMarginTop = Math.floor(window.innerHeight * 0.25);
+
   step.style("height", stepHeight + "px");
   text.selectAll(".step-two").style('height', '200px');
 
   let graphicWidthHeight = width;
 
-  const halfPageHeight = Math.floor(window.innerHeight/2)
-
-  // make margin top for bar chart the size of the map container
-
-
-  // console.warn('graphic Width AND, height', graphic.node().offsetWidth)
-  const graphicMarginTop = Math.floor(window.innerHeight * 0.25);
 // scroll graphic is world map container
   d3.select(".map-graphic-container")
-    .style("width", graphicWidthHeight + "px")
+    .style("width", '100%')
     .style("height", halfPageHeight + "px")
     .style("top", graphicMarginTop + "px");
 
   d3.select(".bar-graphic-container")
     .style('top', halfPageHeight + 'px')
-    .style("width", graphicWidthHeight + "px")
+    .style("width", '100%' + "px")
     .style("height", halfPageHeight + "px")
 
   // Use this to set the distance ofo the first step
-  d3.select(".header-container").style("height", Math.floor(window.innerHeight * 1) + "px");
+  d3.select(".splash-container")
+    .style("height", Math.floor(window.innerHeight * 1) + "px");
 
-
-
-  d3.select(".ussr-svg-container").style("width", textWidth + "px");
   d3.select(".intro-block").style("width", textWidth + "px");
   d3.select(".name-block").style("width", textWidth + "px");
-  d3.select(".ussr-svg").style("height", 200 + "px");
-  d3.select(".ussr-svg").style("width", 200 + "px");
+ 
+  
 } 
