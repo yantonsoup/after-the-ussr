b5cdead58185d4f822ea0f7e3bbf78aacffb7f30
yantonsoup <ygoldobin@gmail.com>
2019-02-24 10:58:11 -0800

    extra route?

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+// // Handles any requests that don't match the ones above
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/public/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
