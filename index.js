9cbf26489631959b084d59239abf9f03f10ddc8f
yantonsoup <ygoldobin@gmail.com>
2019-02-23 18:01:15 -0800

    route tweak

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/public'));
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
